bf6bc799256614b3080ff7e1b2682b2bac449cee
linmin.bd <linmin.bd@bytedance.com>
2024-09-05 21:26:55 +0800

    提交11

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export LD_PRELOAD="$(find /usr/local/lib/python3.8/site-packages/torch.libs -type f -name 'libgo$
+export LD_PRELOAD="$(find /usr/local/lib/python3.8/site-packages/torch.libs -type f -name 'libgomp*')"
 echo $LD_PRELOAD
 python3 server.py
