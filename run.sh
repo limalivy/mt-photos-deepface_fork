b0290b473d07f7f4b779c750bea9754bb7b02eac
linmin.bd <linmin.bd@bytedance.com>
2024-09-05 19:10:23 +0800

    提交9

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-FILE_PATH=$(find /usr/local/lib/python3.8/site-packages/torch.libs -type f -name "libgomp*" 2>/dev/null)
-if [[ -n $FILE_PATH ]]; then
-    # 文件存在，export该路径
-    export LD_PRELOAD=$FILE_PATH
-    echo "File found at: $LD_PRELOAD"
-else
-    echo "File not found."
-fi
+export LD_PRELOAD="$(find /usr/local/lib/python3.8/site-packages/torch.libs -type f -name 'libgo$
+echo $LD_PRELOAD
 python3 server.py
